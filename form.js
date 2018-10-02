6e3ab4a981f68a18b25a69585f342854396d3b4a
lysterjason <jason.lyster@ucalgary.ca>
2018-10-01 18:04:37 -0600

    Pushing JS changes for database

---
+++ form.js
@@ -0,0 +1,28 @@
+var config = {
+  apiKey: "AIzaSyDd1tYh_Imk_hwhGMCv6dN00xYVzXDZiXQ",
+  authDomain: "cpus-71b7f.firebaseapp.com",
+  databaseURL: "https://cpus-71b7f.firebaseio.com",
+  projectId: "cpus-71b7f",
+  storageBucket: "cpus-71b7f.appspot.com",
+  messagingSenderId: "721028204994"
+};
+
+  firebase.initializeApp(config);
+  var members = firebase.database().ref("members");
+  var registerMember = function () {
+
+    var name = ("#name").val();
+    var email = ("#email").val();
+    var ucid = ("#ucid").val();
+
+    members.child(name).set({
+      "name": name,
+      "email": email,
+      "ucid": ucid
+    });
+  };
+  
+  (window).load(function () {
+    ("#signUp").submit(registerMember);
+  
+  });
